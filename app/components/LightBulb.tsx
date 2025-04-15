dbf5af05daa40b3220682045f6c072f57505a5c8
vinayakfr <vinayak.sri@yahoo.com>
2025-04-16 00:22:56 +0530

    feat: create new components

--- app/components/LightBulb.tsx
+++ app/components/LightBulb.tsx
@@ -13,7 +13,7 @@ export const LampContainer = ({
   return (
     <div
       className={cn(
-        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full z-0",
+        "absolute h-screen flex flex-col items-center justify-center overflow-hidden bg-slate-950 w-full z-0",
         className
       )}
     >
@@ -30,7 +30,7 @@ export const LampContainer = ({
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
           // Change the height to 64rem
-          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-rose-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top] animate-flicker"
+          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-orange-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top] animate-flicker"
         >
           {/* Don't touch */}
           <div className="absolute w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
@@ -48,14 +48,14 @@ export const LampContainer = ({
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
           // Change the height to 64rem
-          className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-rose-500 text-white [--conic-position:from_290deg_at_center_top]"
+          className="absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-orange-500 text-white [--conic-position:from_290deg_at_center_top]"
         >
           <div className="absolute  w-40 h-[100%] right-0 bg-slate-950  bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
           <div className="absolute  w-[100%] right-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
         </motion.div>
         <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
         <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-        <div className="absolute inset-auto z-50 h-40 w-[40rem] -translate-y-1/2 rounded-full bg-gradient-to-r from-orange-500 via-red-500 to-rose-500 opacity-50 blur-[5rem]"></div>
+        <div className="absolute inset-auto z-50 h-40 w-[40rem] -translate-y-1/2 rounded-full bg-gradient-to-r from-orange-500 via-orange-500 to-orange-500 opacity-50 blur-[5rem]"></div>
         <motion.div
           initial={{ width: "8rem" }}
           whileInView={{ width: "16rem" }}
@@ -74,13 +74,13 @@ export const LampContainer = ({
             duration: 0.8,
             ease: "easeInOut",
           }}
-          className="absolute inset-auto z-50 h-1 w-[30rem] -translate-y-[7rem] bg-gradient-to-r from-rose-400 via-orange-400 to-red-500 "
+          className="absolute inset-auto z-50 h-1 w-[30rem] -translate-y-[7rem] bg-gradient-to-r from-orange-400 via-orange-100 to-orange-100 "
         ></motion.div>
 
         <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950 "></div>
       </div>
 
-      <div className="relative z-50 flex -translate-y-80 flex-col items-center px-5">
+      <div className="relative z-50 flex -translate-y-80 flex-col g items-center px-5">
         {children}
       </div>
     </div>
